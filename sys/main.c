b0ca84d0c1fe53137eeae642918f2caf7d491993
Devang Saraogi <dsaraog@vclvm178-14.vcl.ncsu.edu>
2025-09-14 23:33:23 -0400

    modified main

--- sys/main.c
+++ sys/main.c
@@ -10,5 +10,17 @@
 int main()
 {
 	kprintf("\n\nHello CSC 501\n\n");
+
+	kprintf("\n\nTask 1: ZFunction\n\n");
+
+	long param = 0xaabbccdd;
+	kprintf("Param: 0x%lx\n", param);
+	kprintf("Expected Result: 0xbbc00dff\n");
+	long result = zfunction(param);
+	kprintf("ZFunction Return: 0x%lx\n", result);
+
+	kprintf("\n\nTask 2: Stack Trace\n\n");
+	printprocstks(0);
+
 	return 0;
 }
